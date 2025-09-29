2427ca7c241b040d2cb599e5b973f1b06766bfe4
Chris Harding <sjrct@users.noreply.github.com>
2025-09-29 18:42:47 -0400

    aliases: create common aliases for bash/fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,43 +1,8 @@
 # vim:foldmethod=marker
 
 if status is-interactive
-  #: Aliases {{{
-  alias ll 'ls -l'
-  alias la 'ls -a'
-  alias v nvim
-  alias kc kubectl
-
-  # Docker composer aliases
-  alias dc 'docker compose'
-  alias dcd 'dc down'
-  alias dcu 'dc up -d'
-  alias dcr 'dc restart'
-  alias dcl 'dc logs'
-
-  # Git aliases
-  alias g 'git'
-  alias gcm 'git commit'
-  alias gch 'git checkout'
-  alias gd 'git diff'
-  alias gl 'git log'
-  alias ga 'git add'
-  alias gpl 'git pull'
-  alias gph 'git push'
-  alias gst 'git status'
-
-  if which xclip &> /dev/null
-    alias xcopy='xclip -in -selection clipboard'
-    alias xpaste='xclip -out -selection clipboard'
-  end
-
-  if [ $TERM = xterm-kitty ]
-    alias ssh='kitten ssh'
-  end
-
-  if type -q eza
-    alias ls='eza --icons=auto'
-  end
-  #: }}}
+  # FIXME
+  source $HOME/.rc/aliases
 
   #: Plugins {{{
   if type -q fisher
