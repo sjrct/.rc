ea08abb48e40db14e4b46b81e6e655979126989c
Chris Harding <sjrct@users.noreply.github.com>
2025-10-03 12:31:39 -0400

    fish: move to abbreviations

--- fish/config.fish
+++ fish/config.fish
@@ -5,8 +5,69 @@ if [ (fish --version | cut -f3 -d' ' | cut -f1 -d.) -lt 4 ]
 end
 
 if status is-interactive
-  # FIXME
-  source $HOME/.rc/aliases
+  #: Abbreviations {{{
+
+  function _subcmd_abbr -a cmd from to pos
+    eval "function _abbr-$cmd-$from -a token
+      if [ (count (commandline | string split -n ' ')) -eq $pos ]
+        echo $to
+      else
+        echo \$token
+      end
+    end"
+    abbr -a $cmd-$from -c $cmd -r $from -f _abbr-$cmd-$from
+  end
+
+  abbr -a ll 'ls -l'
+  abbr -a la 'ls -a'
+  abbr -a v nvim
+  # I've given in...
+  abbr -a vim nvim
+  abbr -a kc kubectl
+
+  # Docker composer abbrevs
+  function _dc_subcmd_abbr -a from to
+    _subcmd_abbr docker $from $to 3
+    abbr -a dc$from "git $to"
+  end
+
+  abbr -a dc 'docker compose'
+  abbr -a docker-compose 'docker compose'
+  _dc_subcmd_abbr d down
+  _dc_subcmd_abbr u 'up -d'
+  _dc_subcmd_abbr r restart
+  _dc_subcmd_abbr l logs
+  _dc_subcmd_abbr e exec
+
+  # Git abbrevs
+  function _git_subcmd_abbr -a from to
+    _subcmd_abbr git $from $to 2
+    abbr -a g$from "git $to"
+  end
+
+  abbr -a g 'git'
+  _git_subcmd_abbr a add
+  _git_subcmd_abbr ch checkout
+  _git_subcmd_abbr cm commit
+  _git_subcmd_abbr d diff
+  _git_subcmd_abbr l log
+  _git_subcmd_abbr ph push
+  _git_subcmd_abbr pl pull
+  _git_subcmd_abbr st status
+  _git_subcmd_abbr sw switch
+
+  type -q xclip && abbr --add xcopy 'xclip -in -selection clipboard'
+  type -q xclip && abbr --add xpaste 'xclip -out -selection clipboard'
+
+  type -q eza && abbr --add ls eza --icons=auto
+
+  [ $TERM = xterm-kitty ] && abbr --add ssh kitten ssh
+
+  # In case I accidentally remember anything from (ba|z)sh...
+  abbr --position=anywhere '$?' '$status'
+  abbr --position=anywhere '$!' '$last_pid'
+  abbr --position=anywhere '$$' '$fish_pid'
+  #: }}}
 
   #: Plugins {{{
   if type -q fisher
@@ -56,11 +117,11 @@ end
 #: }}}
 
 if type -q batcat
-  alias bat='batcat'
+  alias bat=batcat
 end
 
 if type -q fdfind
-  alias fd='fdfind'
+  alias fd=fdfind
 end
 
 [ -e $HOME/.bin ] && fish_add_path $HOME/.bin
